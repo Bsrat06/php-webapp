2c82a408713a3a47c27e125c39b3dbafd090c55b
bsrat mulugeta <bsratmulugetasergawie@gmail.com>
2023-06-01 00:16:48 +0300

    major updates configured!

--- project/templates/home.php
+++ project/templates/home.php
@@ -20,52 +20,85 @@ require_once "auth_check.php";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
 
+    <!-- fontawesome cdn icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
 </head>
 
 <body>
-    <img class="home-image" src="../images/dollar-gill-0V7_N62zZcU-unsplash.jpg">
     <div class="container">
-        <h1 class="title">WELCOME TO</h1>
-        <h1 class="title2">ASTU Digital Resources</h1>
-
         <div class="nav-bar">
             <ul>
                 <div id="nav-home">
                     <li><a href="">HOME</a></li>
                 </div>
                 <div id="nav-getresources">
-                    <li><a href="http://localhost/astuDigitalResource/project/templates/getresources.php">RESOURCES</a></li>
+                    <li><a href="http://localhost/astuDigitalResource/project/templates/getresources.php">RESOURCES</a>
+                    </li>
                 </div>
                 <?php
                 if (!isset($_SESSION)) {
                     echo "<div id=nav-login><li><a href='http://localhost/astuDigitalResource/project/templates/accounts/login.php'>LOG IN</a></li></div>";
                     echo "<div id='nav-signup'><li><a href='http://localhost/astuDigitalResource/project/templates/accounts/signup.php'>SIGN UP</a></li></div>";
                 } else {
-                    if ($_SESSION["user"]=="bsrat@gmail.com"){
+                    if ($_SESSION["user"] == "bsrat@gmail.com") {
                         echo "<div id='nav-upload'><li><a href='upload.php'>UPLOAD</a></li></div>";
-                }
+                    }
                     echo "<div id='nav-logout'><li><a href='http://localhost/astuDigitalResource/project/templates/accounts/logout.php'>LOG OUT</a></li></div>";
                 }
                 ?>
                 <div id="nav-blog">
                     <li><a href="blog_list.php">BLOG</a></li>
                 </div>
-
             </ul>
         </div>
-        <div class="bio">
-            <p class="about"><strong>ABOUT US</strong></p>
+        <h1 class="title">WELCOME TO</h1>
+        <h1 class="title2">ASTU Digital Resources</h1>
+        <img class="home-image" src="../images/dollar-gill-0V7_N62zZcU-unsplash.jpg">
+
+
+
+        <div class="contacts">
+            <a class="facebook" href=""><i class="fa-brands fa-facebook"></i></a>
+            <a class="instagram" href=""><i class="fa-brands fa-instagram"></i></a>
+            <a class="linkedin" href=""><i class="fa-brands fa-linkedin"></i></a>
+            <a class="twitter" href=""><i class="fa-brands fa-twitter"></i></a>
+            <a class="google" href=""><i class="fa-brands fa-google-plus"></i></a>
+        </div>
+        <img class="about_image" src="../images/jess-bailey-K47Tk9IEQPQ-unsplash.jpg">
+        <p class="about"><strong>ABOUT US</strong></p>
+        <div class="about_body">
             <p>This website is designed to help students, teachers, staff members, and anyone who is struggling to find
                 information that is provided by the Adama Science and Technology University.</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repudiandae ratione ipsum provident
-                veritatis voluptas autem ad earum corporis possimus. Impedit laboriosam aut iusto non esse porro
-                facilis? Quisquam, assumenda.</p>
-
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur odio qui unde ipsum accusantium
-                accusamus voluptas vel sint. Eaque modi laborum dolorum cum maiores voluptas adipisci iure nesciunt
-                molestias fuga.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores repudiandae ratione ipsum provident.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fugiat ratione dignissimos, alias ea
+                aliquam totam commodi vero placeat itaque adipisci unde ducimus aliquid non, earum neque optio
+                consectetur repellat.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas numquam aut quasi</p>
+            <p class="about_body2">ipsum dolor sit amet consectetur adipisicing elit. Voluptas numquam aut quasi excepturi culpa
+            ducimus quam incidunt, suscipit eveniet id. Voluptas illum vitae nam quis odit dignissimos quam enim
+            mollitia!</p>
+        </div>
+        <div class="why">
+            <p>Why Choose This Platform ?</p>
         </div>
 
+        <p class="why_body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet saepe doloribus quasi
+            suscipit
+            necessitatibus alias dolorem aut. Labore dignissimos placeat, ullam recusandae fugiat repellat reprehenderit
+            asperiores amet, optio numquam nobis?</p>
+        <p class="why_body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet saepe doloribus quasi
+            suscipit
+            necessitatibus alias dolorem aut. Labore dignissimos placeat, ullam recusandae fugiat repellat reprehenderit
+            asperiores amet, optio numquam nobis?</p>
+        <p class="why_body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet saepe doloribus quasi
+            suscipit
+            necessitatibus alias dolorem aut. Labore dignissimos placeat, ullam recusandae fugiat repellat reprehenderit
+            asperiores amet, optio numquam nobis?</p>
     </div>
 </body>
 
